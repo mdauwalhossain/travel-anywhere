358025baba2f1ddcaa7d0f8b6e75fab23eba47e5
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-10-29 23:43:10 +0600

    form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h2>Contact us page</h2>
+           <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Head Office</h2>
+                        <p class="card-text"> House: SW (I) 1/A, <br />
+                        Road: 100, Gulshan <br />
+                        Dhaka-1212. </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Zonal Office</h2>
+                        <p class="card-text">House: AS (L) 2/Z, <br />
+                        Road: 123, Khulshi <br />
+                        Chattogram-1213. </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Offers/Offers.js
+++ src/Components/Offers/Offers.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Offers = () => {
     return (
         <div>
-            <h1>Offers</h1>
+           <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCYPGABjLERVUPOWXPO2wea_8WGW-qC9Gzew&usqp=CAU" class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLB_gHoEIG3l9r9kYwGnfab3xxiQAJcQ4Qkg&usqp=CAU" class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
